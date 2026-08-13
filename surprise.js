687bf7905e4f2ec24f29d2b53da6d5e4d9f11acc
lakavathveerabhadra-code <lakavathveerabhadra@gmail.com>
2026-08-14 02:09:09 +0530

    Update surprise.js

--- surprise.js
+++ surprise.js
@@ -1,33 +1,49 @@
 function nextPage() {
   document.querySelector(".page").innerHTML = `
-    <h1>🎂 Happy Birthday, My Man ❤️</h1>
-
-    <p>Okay... I'm really sorry I'm late 😭❤️</p>
+    <h1>🎁 I Missed Giving You This On Your Birthday... 🥹</h1>
 
     <p>
-      But I couldn't let your birthday pass without doing
-      something special for you.
+      I know I'm a little late, and I'm really sorry. ❤️
     </p>
 
-    <button onclick="showLetter()">Read your letter 💌</button>
+    <p>
+      I missed giving this to you on your birthday,
+      but I didn't want to let it go without giving you
+      something special.
+    </p>
+
+    <p>
+      So I'm presenting it to you now... ❤️🎁
+    </p>
+
+    <p>
+      I hope this little surprise makes you smile. 🥹
+    </p>
+
+    <button onclick="showLetter()">
+      Open Your Surprise 🎁
+    </button>
   `;
 }
+
 
 function showLetter() {
   document.querySelector(".page").innerHTML = `
     <h1>💌 A Little Letter For You</h1>
 
-    <p>Heyyy, my man ❤️</p>
+    <p>
+      Heyyy, my man ❤️
+    </p>
 
     <p>
-      I know this surprise is a little late... 😭
-      but I really wanted to make something special for you.
+      I know this surprise is a little late...
+      but I really wanted to make something special for you. 🥹
     </p>
 
     <p>
       Honestly, I feel so comfortable whenever I'm with you.
       With you, I can be completely myself without pretending
-      or thinking too much. 🥹❤️
+      or thinking too much. ❤️
     </p>
 
     <p>
@@ -60,7 +76,9 @@ function showLetter() {
       <strong>Happy Birthday, My Man! 🎂🥳❤️</strong>
     </p>
 
-    <button onclick="showMemories()">Our Memories 📸</button>
+    <button onclick="showMemories()">
+      Our Memories 📸
+    </button>
   `;
 }
 
@@ -76,11 +94,11 @@ const photos = [
 ];
 
 const captions = [
-  "One of my favorite memories with you ❤️",
-  "A moment I'll always remember 🥹❤️",
-  "Look at us 😂❤️",
-  "Another beautiful memory with my man 🫶",
-  "A memory I'll keep forever ❤️"
+  "A memory that will always have a special place in my heart ❤️",
+  "Just one of those moments that makes me smile whenever I see it 🥹❤️",
+  "Look at us 😂❤️ Some memories are simply unforgettable.",
+  "Another beautiful moment with my man 🫶❤️",
+  "And this one... a memory I'll always keep close to my heart ❤️🥹"
 ];
 
 
@@ -102,7 +120,9 @@ function showMemories() {
 
     </div>
 
-    <button onclick="nextMemory()">Next Memory ➡️</button>
+    <button onclick="nextMemory()">
+      Next Memory ➡️
+    </button>
   `;
 }
 
@@ -133,7 +153,7 @@ function showGift() {
       <h1>🎁 One Last Surprise...</h1>
 
       <p>
-        You've reached the end of our little memory album 👀❤️
+        You've reached the end of our little memory album. ❤️
       </p>
 
       <p>
@@ -184,6 +204,52 @@ function revealGift() {
         <strong>
           Happy Birthday once again, my man! ❤️
         </strong>
+      </p>
+
+      <button onclick="showFinalMessage()">
+        One More Thing... ❤️
+      </button>
+
+    </div>
+  `;
+}
+
+
+function showFinalMessage() {
+  document.querySelector(".page").innerHTML = `
+    <div class="final-page">
+
+      <h1>❤️ For You, My Darling</h1>
+
+      <img
+        class="my-photo"
+        src="Screenshot_20260625_094330.jpg"
+        alt="A special photo"
+      >
+
+      <h2>Love you, darling ❤️</h2>
+
+      <p>
+        Miss you more than you know. 🥹❤️
+      </p>
+
+      <p>
+        Thank you for being such a beautiful part
+        of my life and for giving me so many memories
+        to treasure.
+      </p>
+
+      <p>
+        No matter how far or how much time passes,
+        you'll always have a special place in my heart. ❤️
+      </p>
+
+      <h2>
+        Happy Birthday, My Man! 🎂🥳❤️
+      </h2>
+
+      <p>
+        With all my love... ❤️
       </p>
 
     </div>
